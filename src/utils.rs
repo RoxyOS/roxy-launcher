34f0f3e8ed9f95de9179d416a825d385a02e3c6e
elysia <3302843873@qq.com>
2026-05-07 17:12:50 +0800

    spawns steam on background

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,10 @@
+use std::thread;
+
 pub fn launch_steam_game(app_id: u32) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("steam://run/{app_id}");
-    open::that(url)?;
+    thread::spawn(|| {
+        // TODO: send result
+        let _ = open::that(url);
+    });
     Ok(())
 }
