db030edc4351c708bfc1f5eadfecc5e2dcb36c98
elysia <3302843873@qq.com>
2026-05-07 17:06:29 +0800

    added GameNotInstalled roxy error

--- src/error.rs
+++ src/error.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub enum RoxyError {
     ProfileDontExist,
+    GameNotInstalled,
     IOError(String),
 }
 
@@ -27,6 +28,7 @@ impl Display for RoxyError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::ProfileDontExist => f.write_str("Profile does not exist"),
+            Self::GameNotInstalled => f.write_str("Steam or Slay the Spire 2 is not installed"),
             Self::IOError(message) => write!(f, "{message}"),
         }
     }
