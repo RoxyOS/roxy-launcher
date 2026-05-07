010b7e8fe21cd05b6d0db21c56df015808af7e9a
elysia <3302843873@qq.com>
2026-05-07 17:30:14 +0800

    fixed title and other bs

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,18 @@
 
 use roxy_launcher::RoxyLauncher;
 
+const TITLE: &str = "Roxy Launcher";
+
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(
-                    &include_bytes!("../assets/favicon-512x512.png")[..],
-                )
-                .expect("Failed to load icon"),
-            ),
+            .with_min_inner_size([300.0, 220.0]),
         renderer: eframe::Renderer::Glow,
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        TITLE,
         native_options,
         Box::new(|cc| Ok(Box::new(RoxyLauncher::new(cc)))),
     )
